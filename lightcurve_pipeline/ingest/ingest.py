b201970744d8f4d9e137e5a09d559f901063562d
Matthew Bourque <bourque@stsci.edu>
2014-11-06 09:58:34 -0500

    Modified make_file_dict to grab the CENWAVE header keyword (based on OBSTYPE) instead of using CENTRWAV.  Also set file_dict keys to lowercase to play nicely with the database interface.

--- lightcurve_pipeline/ingest/ingest.py
+++ lightcurve_pipeline/ingest/ingest.py
@@ -4,12 +4,16 @@ hstlc database.
 
 from __future__ import print_function
 
+import datetime
 import glob
 import os
-import yaml
 
 from astropy.io import fits
+
 from lightcurve_pipeline.settings.settings import SETTINGS
+from lightcurve_pipeline.database.database_interface import engine
+from lightcurve_pipeline.database.database_interface import session
+from lightcurve_pipeline.database.database_interface import Metadata
 
 # -----------------------------------------------------------------------------
 
@@ -30,29 +34,36 @@ def make_file_dict(filename):
     file_dict = {}
 
     # Open image
-    hdulist = fits.open(filename, 'readonly')
-    header = hdulist[0].header
+    with fits.open(filename, 'readonly') as hdulist:
+        header = hdulist[0].header
 
     # Set header keys
-    file_dict['TELESCOP'] = header['TELESCOP']
-    file_dict['INSTRUME'] = header['INSTRUME']
-    file_dict['TARGNAME'] = header['TARGNAME']
-    file_dict['CAL_VER'] = header['CAL_VER']
-    file_dict['OBSTYPE'] = header['OBSTYPE']
-    file_dict['CENTRWV'] = header['CENTRWV']
-    file_dict['APERTURE'] = header['APERTURE']
-    file_dict['DETECTOR'] = header['DETECTOR']
-    file_dict['OPT_ELEM'] = header['OPT_ELEM']
+    file_dict['telescop'] = header['TELESCOP']
+    file_dict['instrume'] = header['INSTRUME']
+    file_dict['targname'] = header['TARGNAME']
+    file_dict['cal_ver'] = header['CAL_VER']
+    file_dict['obstype'] = header['OBSTYPE']
+    file_dict['aperture'] = header['APERTURE']
+    file_dict['detector'] = header['DETECTOR']
+    file_dict['opt_elem'] = header['OPT_ELEM']
+
+    if header['OBSTYPE'] == 'SPECTROSCOPIC':
+        file_dict['cenwave'] = header['CENWAVE']
+    elif header['OBSTYPE'] == 'IMAGING':
+        file_dict['cenwave'] = 0
 
     if header['INSTRUME'] == 'COS':
-        file_dict['FPPOS'] = header['FPPOS']
+        file_dict['fppos'] = header['FPPOS']
     elif header['INSTRUME'] == 'STIS':
-        file_dict['FPPOS'] = 0
+        file_dict['fppos'] = 0
 
-    # Set path keys
+    # Set image metadata keys
+    file_dict['filename'] = os.path.basename(filename)
     root_dst = SETTINGS['filesystem_dir']
     file_dict['src'] = filename
-    file_dict['dst'] = os.path.join(root_dst, file_dict['TARGNAME'])
+    file_dict['path'] = os.path.join(root_dst, file_dict['targname'])
+    today = datetime.datetime.today()
+    file_dict['ingest_date'] = datetime.datetime.strftime(today, '%Y-%m-%d')
 
     return file_dict
 
@@ -72,8 +83,6 @@ def update_database(file_dict):
 
     """
 
-    pass
-
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 
@@ -81,7 +90,7 @@ if __name__ == '__main__':
 
     filelist = glob.glob(os.path.join(SETTINGS['ingest_dir'], '*.fits*'))
 
-    for file_to_ingest in filelist:
+    for file_to_ingest in filelist[0:1]:
 
         print('Ingesting {}'.format(file_to_ingest))
 
