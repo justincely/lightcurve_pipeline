da4db2f13f74bc7a72ef76d03e4b12e0227e6723
Matthew Bourque <bourque@stsci.edu>
2015-03-27 15:04:17 -0400

    Updated script names to refelct new hstlc-specific names.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
                    'Topic :: Software Development :: Libraries :: Python Modules'],
     packages = find_packages(),
     requires = ['numpy', 'astropy', 'sqlalchemy', 'pyyaml'],
-    scripts = ['scripts/download', 'scripts/ingest', 'scripts/reset_database', 'scripts/reset_filesystem'],
+    scripts = ['scripts/download_hstlc', 'scripts/ingest_hstlc', 'scripts/reset_hstlc_database', 'scripts/reset_hstlc_filesystem'],
     data_files = [('lightcurve_pipeline/utils/', ['lightcurve_pipeline/utils/config.yaml'])]
     )
